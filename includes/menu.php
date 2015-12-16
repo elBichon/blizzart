fdd741cdb5050393a1ba7dfdca8cc0c938841522
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-17 00:10:16 +0100

    le menu en page include

---
+++ includes/menu.php
@@ -0,0 +1,24 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div id="main" class="container-fluid">
+        <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+        </button>
+        </div>
+    <div>
+	  <div class="collapse navbar-collapse" id="myNavbar">
+	    <ul class="nav navbar-nav" id="menu">
+	      <li><a href="#section1" class="scrollTo"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
+          <li><a href="#section2" class="scrollTo"><span class="glyphicon glyphicon-fire"></span> le festival</a></li>
+          <li><a href="#section3" class="scrollTo"><span class="glyphicon glyphicon-fire"></span> programme</a></li>
+	      <li><a href="#section4" class="scrollTo"><span class="glyphicon glyphicon-briefcase"></span> Entreprises</a></li>
+	      <li><a href="#section5" class="scrollTo"><span class="glyphicon glyphicon-user"></span> L'&eacutequipe</a></li>
+	      <li><a href="#section6" class="scrollTo"><span class="glyphicon glyphicon-envelope"></span> Contact</a></li>
+	    </ul>
+	  </div>
+	</div>
+      </div>
+    </nav>
+
