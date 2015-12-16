8f40c0cbd79c8a10a260ac7621185631edaa3b83
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-17 00:25:28 +0100

    une version modifiée du menu avec le programme

--- includes/menu.php
+++ includes/menu.php
@@ -12,7 +12,7 @@
 	    <ul class="nav navbar-nav" id="menu">
 	      <li><a href="#section1" class="scrollTo"><span class="glyphicon glyphicon-home"></span> Accueil</a></li>
           <li><a href="#section2" class="scrollTo"><span class="glyphicon glyphicon-fire"></span> le festival</a></li>
-          <li><a href="#section3" class="scrollTo"><span class="glyphicon glyphicon-fire"></span> programme</a></li>
+          <li><a href="#section3" class="scrollTo"><span class="glyphicon glyphicon-align-justify"></span> programme</a></li>
 	      <li><a href="#section4" class="scrollTo"><span class="glyphicon glyphicon-briefcase"></span> Entreprises</a></li>
 	      <li><a href="#section5" class="scrollTo"><span class="glyphicon glyphicon-user"></span> L'&eacutequipe</a></li>
 	      <li><a href="#section6" class="scrollTo"><span class="glyphicon glyphicon-envelope"></span> Contact</a></li>
