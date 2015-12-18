c5c6e77142eb27b96882fb0cd362e2c6558506d1
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-18 21:01:06 +0100

    changement pour afficher la bonne image

--- includes/includesProgramme.php
+++ includes/includesProgramme.php
@@ -1,11 +1,12 @@
+<div id="includesProgramme">
 <?php
-    $reponse = $bdd->query('SELECT nom FROM images WHERE id=4');
+    $reponse = $bdd->query('SELECT nom FROM images WHERE id=2');
     while ($donnees = $reponse->fetch())
     {
         echo '<div id="c1141" class="col-sm-2 col-xs-2"><img id="sponso1" class="img-responsive" src="'. $donnees['nom'] .'" alt="programme du festival"></div>';
     }
     $reponse->closeCursor();
     ?>
-
+</div>
 
 
