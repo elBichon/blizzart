f72adfdcd9c04beecb72a42d3d0de86a41644536
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-18 20:00:10 +0100

    le script qui permet de rendre visible le programme du festival

---
+++ includes/includesProgramme.php
@@ -0,0 +1,11 @@
+<?php
+    $reponse = $bdd->query('SELECT nom FROM images WHERE id=4');
+    while ($donnees = $reponse->fetch())
+    {
+        echo '<div id="c1141" class="col-sm-2 col-xs-2"><img id="sponso1" class="img-responsive" src="'. $donnees['nom'] .'" alt="programme du festival"></div>';
+    }
+    $reponse->closeCursor();
+    ?>
+
+
+
