c3930f5febd5b1699ef00f7caf6d7d75b2b09503
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-21 10:27:10 +0100

    dernière modification du programme

--- includes/includesProgramme.php
+++ includes/includesProgramme.php
@@ -1,12 +1,11 @@
-<div id="includesProgramme">
+<?php include("connect.php"); ?>
 <?php
-    $reponse = $bdd->query('SELECT nom FROM images WHERE id=2');
+    $reponse = $bdd->query('SELECT * FROM images WHERE id=2');
     while ($donnees = $reponse->fetch())
     {
         echo '<div id="c1141" class="col-sm-2 col-xs-2"><img id="sponso1" class="img-responsive" src="'. $donnees['nom'] .'" alt="programme du festival"></div>';
     }
     $reponse->closeCursor();
-    ?>
-</div>
+?>
 
 
