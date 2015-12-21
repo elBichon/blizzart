2f7d4455f2a6a477cbb5d09bfa0ea4eaffcffd15
elBichon <reuglewiczjeanedouard@gmail.com>
2015-12-21 10:24:02 +0100

    ajout de la fonction d'affichage du programme selon la date

--- JS/functions.js
+++ JS/functions.js
@@ -1,4 +1,4 @@
- <script>
+
     $(document).ready(function() {
 		$('.scrollTo').click( function() {
 			var page = $(this).attr('href');
@@ -7,9 +7,9 @@
 			return false;
 		});
     });
-    </script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -17,4 +17,14 @@
   ga('create', 'UA-70463663-1', 'auto');
   ga('send', 'pageview');
 
-</script>
+
+function dateAffichage(){
+if (myDate.getYear()===myTimestamp.getYear() && myDate.getMonth()===myTimestamp.getMonth() && myDate.getDay()===myTimestamp.getDay()){alert("plop");
+    $(document).ready(function(){alert("plop");
+                      console.log('test');
+                      alert("plop");
+                      $('#pgm').load("http://localhost/blizzart/includes/includesProgramme.php", function() {alert("plop");});
+                      });
+    }
+}
+
